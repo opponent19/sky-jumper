5000c6eb5a55a43ab65fa24e885bcb210094dbe3
Mahendra Yadav <138255250+opponent19@users.noreply.github.com>
2024-04-12 00:49:59 +0530

    project-HTML-CSS-JS

---
+++ company-project/scripts.js
@@ -0,0 +1,5 @@
+
+document.getElementById('ticket').addEventListener('click', () => console.log("book tickets"))
+
+
+document.getElementById('party').addEventListener('click', () => console.log("get ready for party"))
